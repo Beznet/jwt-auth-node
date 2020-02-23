a56fdad1e5ba1395cad42d32d07784c4a7674a3c
Bennett Dungan <bennettdungan@gmail.com>
2020-02-23 14:49:29 -0600

    created login mutation that checks email/password and returns a JWT accessToken

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,7 +1,13 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql'
-import {hash} from 'bcryptjs'
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
 import { User } from './entity/User';
+import {sign} from 'jsonwebtoken'
 
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
 @Resolver() 
 export class UserResolver {
 
@@ -14,6 +20,30 @@ export class UserResolver {
     users() {
         return User.find();
     }
+
+    @Mutation(()=> LoginResponse)
+        async login(
+            @Arg('email') email: string,
+            @Arg('password') password: string
+        ): Promise<LoginResponse> {
+            const user = await User.findOne( { where: { email }} )
+
+            if (!user) {
+                throw new Error('Could Not Find User')
+            }
+
+            const valid = await compare(password, user.password)
+
+            if(!valid) {
+                throw new Error('bad password')
+            }
+
+            // login successful
+
+            return {
+                accessToken: sign({userId: user.id}, 'asdf', {expiresIn: '15min'})
+            }
+        }
 
     @Mutation(()=> Boolean)
     async register(
